265039df317bd2dc1158da653d9f49c9fbb614b3
ViniCatureba <vinicius.bigao14@gmail.com>
2022-08-27 09:30:04 -0300

    updates

---
+++ classes/003variables/003declareavar.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// if you ever need to declarate a variable outside a function you gotta you var, cannot use the short declaration.
+// DECLARE the variable "y"
+// ASSIGN the value 43
+// declare & assign = initilization
+var y = 43
+
+//DECLARE there is a variable with the IDENTIFIER "z"
+//and that the VARIABLE  with the IDENTIFIER "z" is of TYPE int
+// ASSIGNS the ZERO value of TYPE in to "z"
+// false for booleans, 0 for integers, 0.0 for floats, "" for strings,
+// and nil for pointers, functions, interfaces, slices, channels, and maps.
+var z int
+
+func main() {
+	//short declaration operator
+	// DECLARE a variable and ASSIGN a VALUE
+	x := 42
+	println(x)
+
+	fmt.Println(y)
+
+	foo()
+	println(z)
+}
+
+func foo() {
+	fmt.Println("again:", y)
+}
