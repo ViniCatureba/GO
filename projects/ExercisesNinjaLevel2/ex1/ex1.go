8e45ebc06228ccf31422d5190166e42ca9c3a6cf
ViniCatureba <vinicius.bigao14@gmail.com>
2022-08-29 22:15:04 -0300

    updates

---
+++ projects/ExercisesNinjaLevel2/ex1/ex1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 32
+
+	fmt.Printf("%d\n%b\n%#x", x, x, x)
+}
---
+++ projects/ExercisesNinjaLevel2/ex2/ex2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	g := (10 == 2)
+	h := (10 <= 2)
+	i := (10 >= 2)
+	j := (10 != 2)
+	k := (10 < 2)
+	l := (10 > 2)
+	fmt.Println(g, h, i, j, k, l)
+
+}
