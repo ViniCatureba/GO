e325ad3a3ac73463b3303bf4f014f25761214693
ViniCatureba <vinicius.bigao14@gmail.com>
2022-09-02 18:44:18 -0300

    updates

--- classes/023conditionalLogicOprt/023conditionalLogicOprt.go
+++ classes/023conditionalLogicOprt/023conditionalLogicOprt.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	fmt.Println(true && true) //&& means and
+	fmt.Println(true && false) 
+	fmt.Println(true || true) // || means or
+	fmt.Println(true || false)
+	fmt.Println(!true) // ! means different
 }
