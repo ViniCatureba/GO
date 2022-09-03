51d98f6dcffcdcd035d71e3f3c5963899a44a8e9
ViniCatureba <vinicius.bigao14@gmail.com>
2022-09-02 22:26:35 -0300

    updates

---
+++ projects/ExercisesNinjaLevel3/ex8/ex8.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	switch {
+	case false:
+		fmt.Println("it is false")
+	case true:
+		fmt.Println("it is true")
+	}
+}
---
+++ projects/ExercisesNinjaLevel3/ex9/ex9.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	favSport := "snowboard"
+	switch favSport {
+	case "bball":
+		fmt.Println("bbal")
+	case "voleyball":
+		fmt.Println("voleyball")
+	case "snowboard":
+		fmt.Println("snowboard")
+
+	}
+}
