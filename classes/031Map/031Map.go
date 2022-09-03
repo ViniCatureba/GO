2b935ca5bf03df6324fedb605cf2a770c3320a2a
ViniCatureba <vinicius.bigao14@gmail.com>
2022-09-03 19:22:01 -0300

    update

--- classes/031Map/031Map.go
+++ classes/031Map/031Map.go
@@ -19,4 +19,19 @@ func main() {
 	if v, ok := m["Miss Moneypenny"]; ok { // this if statement will verify if this map is true and if is will print.
 		fmt.Println("THIS IS THE IF PRINT ", v)
 	}
+
+	m["todd"] = 33 // add a key in the map
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	delete(m, "James")
+	fmt.Println(m)
+
+	if t, d := m["Miss Moneypenny"]; d {
+		fmt.Println("value: ", t)
+		delete(m, "Miss Moneypenny")
+	}
+	fmt.Println(m)
 }
