1394b79871ec853d0db4cd9ee09b4f5b2fefda0c
ViniCatureba <vinicius.bigao14@gmail.com>
2022-09-05 12:39:10 -0300

    update

--- projects/ExercisesNinjaLevel4/ex10/ex10.go
+++ projects/ExercisesNinjaLevel4/ex10/ex10.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	x := map[string][]string{
+		"Vinicius_Catureba": []string{"Sorvete", "cafe", "snowboard"},
+		"Joao_Marques":      []string{"Pastel", "praia", "viajar"},
+		"Igor_Reis":         []string{"Gym", "carro", "pagode"},
+	}
+	fmt.Println(x)
+	fmt.Println(len(x))
+
+	x["Julio"] = []string{
+		"carro", "vacilar", "musica",
+	}
+
+	for k, v := range x {
+		fmt.Println("This is the record for ", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+
+	delete(x, "Julio")
+
+	for k, v := range x {
+		fmt.Println("This is 2nd the record for ", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+		}
+	}
+
 }
