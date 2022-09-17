18e78f3d5d3db47be30e5fc6ec5cfcd5aabf3ea9
ViniCatureba <vinicius.bigao14@gmail.com>
2022-09-17 08:53:57 -0300

    updates

---
+++ classes/037UnfurlingASlice/037UnfurlingASlice.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	xi := []int{2, 3, 4, 5, 6, 7, 8, 9}
+	x := sum(xi...)
+	fmt.Println("The total is,", x)
+}
+
+func sum(x ...int) int {
+	fmt.Println("Hello, playgournd")
+
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index possitions", i, " we are now adding", v, " to the tootal wich is now ", sum)
+	}
+
+	return sum
+}
